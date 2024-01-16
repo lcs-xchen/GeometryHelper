9e8710b16063ca008e2ed6b1b7f74357fecce181
Cindy Chen <xchen@lcs.on.ca>
2024-01-16 14:08:57 -0500

    Initial Commit

---
+++ GeometryHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometryHelper
+//
+//  Created by Cindy Chen on 2024-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Cindy Chen on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
