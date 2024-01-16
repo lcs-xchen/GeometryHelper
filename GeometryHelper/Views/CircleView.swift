99bcec0f9bbda932c05fdd636a8e69666c4b2e7a
Cindy Chen <xchen@lcs.on.ca>
2024-01-16 14:12:50 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ GeometryHelper/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometryHelper
+//
+//  Created by Cindy Chen on 2024-01-16.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
